abd6be955a5f48614a41897be32a5a10dbbe7b78
LKR1x <171375882+LKR1x@users.noreply.github.com>
2025-08-14 18:52:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+    base: '/pebble-ai-landing-page/',
   plugins: [react()],
   assetsInclude: ['**/*.riv'], // 👈 Tell Vite to treat .riv as an asset
 })
